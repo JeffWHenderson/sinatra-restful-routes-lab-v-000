9ee685b5ed2ee3b8a029f23ee3c702f9d8af4ac7
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-03 19:59:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  
+  get '/' do
+    redirect '/recipes'
+  end
   get '/recipes' do
-    #@recipes = Recipe.all
     erb :index
   end
 
